ddca9cde460c01c1e2c6185bf818f76ef40dcd84
JulsZhovanyk <julia.zhovanyk@gmail.com>
2022-12-10 13:27:03 +0200

    creating simpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const createGallery = (items) => {
+  const createGalleryEl = items
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a>`
+    )
+    .join("");
+
+  gallery.insertAdjacentHTML("afterbegin", createGalleryEl);
+};
+
+createGallery(galleryItems);
+
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
+
+const instance = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+gallery.addEventListener("click", onImgClick);
 
 console.log(galleryItems);
